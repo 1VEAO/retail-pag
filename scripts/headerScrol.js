426e9f46b2c953e7639add9268cb8509ee770015
Victor Alarcon <alarconolmedo99@gmail.com>
2024-04-12 18:31:49 -0500

    banner

---
+++ scripts/headerScrol.js
@@ -0,0 +1,16 @@
+const barScrol = document.getElementsByClassName('bar')[0];
+const navRrss = document.getElementsByClassName('nav-rrss')[0];
+
+barScrol.addEventListener('click', function scrol() {
+ if (navRrss.style.top == '-80px') {
+  navRrss.style.setProperty('top', '120px')
+ } else {
+  navRrss.style.setProperty('top', '-80px')
+ }
+})
+
+
+// prueba promociones
+
+const bannerProm = document.getElementsByClassName('banner-promociones')[0];
+// bannerProm.innerHTML=('<img src="./assets/hero_bg.png" alt="logo">')
