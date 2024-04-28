762940b3bea001b6c086904b152cd1a68f312b2a
Victor Alarcon <alarconolmedo99@gmail.com>
2024-04-27 22:54:34 -0500

    creacion de tarjetas renderizado personalizaco

--- scripts/product-card.js
+++ scripts/product-card.js
@@ -1,20 +1,70 @@
 let cardCont = document.getElementsByClassName('card-container')[0];
-const search = document.getElementsByClassName('search-inp')[0]
+const search = document.getElementsByClassName('search-inp')[0];
+
+const createCard = () => {
+ for (const product of productsArray) {
+  const link = document.createElement('a');
+  const classCard = document.createElement('div');
+  const imgMok = document.createElement('img');
+  const classInfoCard = document.createElement('div');
+  const nameProduct = document.createElement('h1');
+  const modelProduct = document.createElement('span');
+  const classPrice = document.createElement('div');
+  const productPrice = document.createElement('h1');
+  const discount = document.createElement('span');
+  const igv = document.createElement('span');
+  link.href = "./rutas/details.html"
+  classCard.className = 'card'
+  classInfoCard.className = 'info-card'
+  classPrice.className = 'price'
+
+  imgMok.src = product.img
+  imgMok.style.maxWidth = '250px'
+
+  nameProduct.textContent = product.productName
+
+  modelProduct.textContent = product.productModel
+
+  productPrice.textContent = product.productPrice
+
+  discount.textContent = product.discount
+
+  igv.textContent = product.igv
+
+  classPrice.appendChild(productPrice)
+  classPrice.appendChild(discount)
+
+  classInfoCard.appendChild(nameProduct)
+  classInfoCard.appendChild(modelProduct)
+  classInfoCard.appendChild(classPrice)
+  classInfoCard.appendChild(igv)
+
+  classCard.appendChild(imgMok)
+  classCard.appendChild(classInfoCard)
+
+  link.appendChild(classCard)
+
+  cardCont.appendChild(link)
+ }
+}
+
 
 class cardItem {
  constructor(img, productName, productModel, productPrice, discount, igv) {
   this.img = img,
-   this.productName = productName,
-   this.productModel = productModel,
-   this.productPrice = productPrice,
-   this.discount = discount,
-   this.igv = igv
+  this.productName = productName,
+  this.productModel = productModel,
+  this.productPrice = productPrice,
+  this.discount = discount,
+  this.igv = igv
  }
 }
 
-const productsArray = [new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP")]
+const productsArray = [new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone XR", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone 12 Pro Max", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Mackbook 12'2", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone SE", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone 8", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP")]
 
-for (const product of productsArray) {
+createCard()
+
+const createUnicCard = (pos) => {
  const link = document.createElement('a');
  const classCard = document.createElement('div');
  const imgMok = document.createElement('img');
@@ -25,44 +75,48 @@ for (const product of productsArray) {
  const productPrice = document.createElement('h1');
  const discount = document.createElement('span');
  const igv = document.createElement('span');
- link.href = "./rutas/details.html"
- classCard.className = 'card'
- classInfoCard.className = 'info-card'
- classPrice.className = 'price'
+ link.href = "./rutas/details.html";
+ classCard.className = 'card';
+ classInfoCard.className = 'info-card';
+ classPrice.className = 'price';
 
- imgMok.src = product.img
- imgMok.style.maxWidth = '250px'
+ imgMok.src = productsArray[pos].img;
+ imgMok.style.maxWidth = '250px';
 
- nameProduct.textContent = product.productName
+ nameProduct.textContent = productsArray[pos].productName;
 
- modelProduct.textContent = product.productModel
+ modelProduct.textContent = productsArray[pos].productModel;
 
- productPrice.textContent = product.productPrice
+ productPrice.textContent = productsArray[pos].productPrice;
 
- discount.textContent = product.discount
+ discount.textContent = productsArray[pos].discount;
 
- igv.textContent = product.igv
+ igv.textContent = productsArray[pos].igv;
 
- classPrice.appendChild(productPrice)
- classPrice.appendChild(discount)
+ classPrice.appendChild(productPrice);
+ classPrice.appendChild(discount);
 
- classInfoCard.appendChild(nameProduct)
- classInfoCard.appendChild(modelProduct)
- classInfoCard.appendChild(classPrice)
- classInfoCard.appendChild(igv)
+ classInfoCard.appendChild(nameProduct);
+ classInfoCard.appendChild(modelProduct);
+ classInfoCard.appendChild(classPrice);
+ classInfoCard.appendChild(igv);
 
- classCard.appendChild(imgMok)
- classCard.appendChild(classInfoCard)
+ classCard.appendChild(imgMok);
+ classCard.appendChild(classInfoCard);
 
- link.appendChild(classCard)
-
- cardCont.appendChild(link)
+ link.appendChild(classCard);
 }
-
 
 // busqueda y filtrado
 
-
-search.addEventListener('keyup', function name(e) {
-
+search.addEventListener('keyup', function validate(e) {
+ if (e.key == 'Enter') {
+  if (search.value.length > 0) {
+   console.log(productsArray[search.value]);
+   cardCont.innerHTML = ``
+   createUnicCard(search.value)
+  } else {
+   alert('no')
+  }
+ }
 })
---
+++ scripts/product-card_new.js
@@ -0,0 +1,187 @@
+{/* <a href="./rutas/details.html">
+      <div class="card"><img src="./assets/mock2.jpg" style="max-width: 250px;">
+        <div class="info-card">
+          <h1>MackBook Pro 15'4</h1><span>Space Gray</span>
+          <div class="price">
+            <h1>$750.000</h1><span>50% Off</span>
+          </div><span>Incluye impuestos Pais y percepcion AFIP</span>
+        </div>
+      </div>
+    </a> */}
+
+
+
+const cardContainer = document.createElement('section')
+cardContainer.className = 'card-container'
+const cardContainerEspe = document.createElement('section')
+cardContainerEspe.className = 'card-container'
+
+const searchInput = document.getElementsByClassName('search-inp')[0];
+const mody = document.querySelector('main')
+
+
+class cardItem {
+ constructor(img, productName, productModel, productPrice, discount, igv) {
+  this.img = img,
+   this.productName = productName,
+   this.productModel = productModel,
+   this.productPrice = productPrice,
+   this.discount = discount,
+   this.igv = igv
+ }
+};
+
+const productsArray = [new cardItem('./assets/mock2.jpg', "MackBook Pro 15'4", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone XR", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone 12 Pro Max", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Mackbook 12'2", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone SE", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP"), new cardItem('./assets/mock2.jpg', "Iphone 8", "Space Gray", '$750.000', "50% Off", "Incluye impuestos Pais y percepcion AFIP")];
+
+const createCard = () => {
+ // const cardContainer = document.createElement('section')
+ // cardContainer.className = 'card-container'
+ for (const product of productsArray) {
+  const link = document.createElement('a');
+  const classCard = document.createElement('div');
+  const imgMok = document.createElement('img');
+  const classInfoCard = document.createElement('div');
+  const nameProduct = document.createElement('h1');
+  const modelProduct = document.createElement('span');
+  const classPrice = document.createElement('div');
+  const productPrice = document.createElement('h1');
+  const discount = document.createElement('span');
+  const igv = document.createElement('span');
+  link.href = "./rutas/details.html"
+  classCard.className = 'card'
+  classInfoCard.className = 'info-card'
+  classPrice.className = 'price'
+
+  imgMok.src = product.img
+  imgMok.style.maxWidth = '250px'
+
+  nameProduct.textContent = product.productName
+
+  modelProduct.textContent = product.productModel
+
+  productPrice.textContent = product.productPrice
+
+  discount.textContent = product.discount
+
+  igv.textContent = product.igv
+
+  classPrice.appendChild(productPrice)
+  classPrice.appendChild(discount)
+
+  classInfoCard.appendChild(nameProduct)
+  classInfoCard.appendChild(modelProduct)
+  classInfoCard.appendChild(classPrice)
+  classInfoCard.appendChild(igv)
+
+  classCard.appendChild(imgMok)
+  classCard.appendChild(classInfoCard)
+
+  link.appendChild(classCard)
+
+  cardContainer.appendChild(link)
+  mody.appendChild(cardContainer)
+ }
+}
+createCard()
+
+
+searchInput.addEventListener('keyup', function validate(e) {
+ if (e.key == "Enter") {
+  if (searchInput.value > 0) {
+   // const cardContainerEspe = document.createElement('section')
+   // cardContainerEspe.className = 'card-container'
+
+   const link = document.createElement('a');
+   const classCard = document.createElement('div');
+   const imgMok = document.createElement('img');
+   const classInfoCard = document.createElement('div');
+   const nameProduct = document.createElement('h1');
+   const modelProduct = document.createElement('span');
+   const classPrice = document.createElement('div');
+   const productPrice = document.createElement('h1');
+   const discount = document.createElement('span');
+   const igv = document.createElement('span');
+   link.href = "./rutas/details.html"
+   classCard.className = 'card'
+   classInfoCard.className = 'info-card'
+   classPrice.className = 'price'
+
+   imgMok.src = productsArray[searchInput.value].img
+   imgMok.style.maxWidth = '250px'
+
+   nameProduct.textContent = productsArray[searchInput.value].productName
+
+   modelProduct.textContent = productsArray[searchInput.value].productModel
+
+   productPrice.textContent = productsArray[searchInput.value].productPrice
+
+   discount.textContent = productsArray[searchInput.value].discount
+
+   igv.textContent = productsArray[searchInput.value].igv
+
+   classPrice.appendChild(productPrice)
+   classPrice.appendChild(discount)
+
+   classInfoCard.appendChild(nameProduct)
+   classInfoCard.appendChild(modelProduct)
+   classInfoCard.appendChild(classPrice)
+   classInfoCard.appendChild(igv)
+
+   classCard.appendChild(imgMok)
+   classCard.appendChild(classInfoCard)
+
+   link.appendChild(classCard)
+
+   cardContainerEspe.appendChild(link)
+   mody.replaceChild(cardContainerEspe, cardContainer)
+
+  } 
+ }
+})
+
+// const cardContainerEspe = document.createElement('section')
+// cardContainerEspe.className = 'card-container'
+
+// const link = document.createElement('a');
+// const classCard = document.createElement('div');
+// const imgMok = document.createElement('img');
+// const classInfoCard = document.createElement('div');
+// const nameProduct = document.createElement('h1');
+// const modelProduct = document.createElement('span');
+// const classPrice = document.createElement('div');
+// const productPrice = document.createElement('h1');
+// const discount = document.createElement('span');
+// const igv = document.createElement('span');
+// link.href = "./rutas/details.html"
+// classCard.className = 'card'
+// classInfoCard.className = 'info-card'
+// classPrice.className = 'price'
+
+// imgMok.src = productsArray[searchInput.value].img
+// imgMok.style.maxWidth = '250px'
+
+// nameProduct.textContent = productsArray[searchInput.value].productName
+
+// modelProduct.textContent = productsArray[searchInput.value].productModel
+
+// productPrice.textContent = productsArray[searchInput.value].productPrice
+
+// discount.textContent = productsArray[searchInput.value].discount
+
+// igv.textContent = productsArray[searchInput.value].igv
+
+// classPrice.appendChild(productPrice)
+// classPrice.appendChild(discount)
+
+// classInfoCard.appendChild(nameProduct)
+// classInfoCard.appendChild(modelProduct)
+// classInfoCard.appendChild(classPrice)
+// classInfoCard.appendChild(igv)
+
+// classCard.appendChild(imgMok)
+// classCard.appendChild(classInfoCard)
+
+// link.appendChild(classCard)
+
+// cardContainerEspe.appendChild(link)
+// mody.appendChild(cardContainerEspe)
